228b9afd7c52c954d5f5b30948db10a812db0166
deergod <CommonTroll@live.co.uk>
2023-03-29 16:57:45 +0100

    updating to use mathjs, multi-line screen, and correct mathematical operators

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import * as math from "mathjs";
 import './App.css';
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
@@ -6,115 +7,58 @@ import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
 
 const btnValues = [
-    ["C", "+-", "%", "/"],
-    [7, 8, 9, "X"],
+    ["C", "Ans", "%", "÷"],
+    [7, 8, 9, "×"],
     [4, 5, 6, "-"],
     [1, 2, 3, "+"],
     [0, ".", "="],
 ];
 
 const toLocaleString = (num) =>
-    String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+    String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1,");
 
-const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+const formatExpr = (expr) => expr.replace(/×/g, "*").replace(/÷/g, "/");
 
 
 function App() {
     let [calc, setCalc] = useState({
-        sign: "",
-        num: 0,
-        res: 0,
+        expr: "",
+        res: 0
     });
 
-    const numClickHandler = (e) => {
+    const clickHandler = (e) => {
         e.preventDefault();
-        const value = e.target.innerHTML;
+        const value = e.target.textContent;
+        let newExpr = calc.expr + value;
+        console.log(`value: ${value} current expr: ${calc.expr} new expr: ${newExpr}`);
 
-        if (removeSpaces(calc.num).length < 16) {
-            setCalc({
+        setCalc({
                 ...calc,
-                num:
-                    calc.num === 0 && value === "0"
-                        ? "0"
-                        : removeSpaces(calc.num) % 1 === 0
-                        ? toLocaleString(Number(removeSpaces(calc.num + value)))
-                        : toLocaleString(calc.num + value),
-                res: !calc.sign ? 0 : calc.res,
-            });
-        }
-    }
-
-    const commaClickHandler = (e) => {
-        e.preventDefault();
-        const value = e.target.innerHTML;
-
-        setCalc({
-            ...calc,
-            num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
-        });
-    }
-
-    const signClickHandler = (e) => {
-        e.preventDefault();
-        const value = e.target.innerHTML;
-
-        setCalc({
-            ...calc,
-            sign: value,
-            res: !calc.res && calc.num ? calc.num : calc.res,
-            num: 0,
-        });
+                expr: newExpr,
+                res: calc.res
+                });
     }
 
     const equalsClickHandler = () => {
-        if (calc.sign && calc.num) {
-            const math = (a, b, sign) =>
-                sign === "+"
-                    ? a + b
-                    : sign === "-"
-                    ? a - b
-                    : sign === "X"
-                    ? a * b
-                    : a / b;
-
-            setCalc({
+        setCalc({
                 ...calc,
-                res:
-                    calc.num === "0" && calc.sign === "/"
-                        ? "Can't divide with 0"
-                        : math(Number(calc.res), Number(calc.num), calc.sign),
-                sign: "",
-                num: 0,
+                expr: calc.expr,
+                res: math.evaluate(formatExpr(calc.expr))
             });
-        }
     }
 
-    const invertClickHandler = () => {
+    const ansClickHandler = () => {
         setCalc({
             ...calc,
-            num: calc.num ? calc.num * -1 : 0,
-            res: calc.res ? calc.res * -1 : 0,
-            sign: "",
-        });
-    }
-
-    const percentClickHandler = () => {
-        let num = calc.num ? parseFloat(calc.num) : 0;
-        let res = calc.res ? parseFloat(calc.res) : 0;
-
-        setCalc({
-            ...calc,
-            num: (num /= Math.pow(100, 1)),
-            res: (res /= Math.pow(100, 1)),
-            sign: "",
+            expr: calc.res,
+            res: 0
         });
     }
 
     const resetClickHandler = () => {
         setCalc({
             ...calc,
-            sign: "",
-            num: 0,
+            expr: "",
             res: 0,
         });
     }
@@ -122,7 +66,7 @@ function App() {
     return (
         <div className="App">
             <Wrapper>
-                <Screen value={calc.num ? calc.num : calc.res} />
+                <Screen value1={calc.expr} value2={calc.res !== 0 ? toLocaleString(calc.res) : ""}/>
                 <ButtonBox>
                     {btnValues.flat().map((btn, i) => {
                         return (
@@ -133,17 +77,11 @@ function App() {
                                 onClick={
                                     btn === "C"
                                         ? resetClickHandler
-                                        : btn === "+-"
-                                        ? invertClickHandler
-                                        : btn === "%"
-                                        ? percentClickHandler
+                                        : btn === "Ans"
+                                        ? ansClickHandler
                                         : btn === "="
                                         ? equalsClickHandler
-                                        : btn === "/" || btn === "X" || btn === "-" || btn === "+"
-                                        ? signClickHandler
-                                        : btn === "."
-                                        ? commaClickHandler
-                                        : numClickHandler
+                                        : clickHandler
                                 }
                             />
                         );
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,10 +1,11 @@
 import { Textfit } from "react-textfit";
 import "./Screen.css";
 
-const Screen = ({ value }) => {
+const Screen = ({ value1, value2 }) => {
   return (
-    <Textfit className="screen" mode="single" max={70}>
-      {value}
+    <Textfit className="screen" mode="multi" max={70}>
+      <p id="line1">{value1}</p>
+      <p id="line2">{value2}</p>
     </Textfit>
   );
 };
